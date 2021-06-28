502f2090e97ba1298962e4f7d69e5a95d24203e8
Reza Qorbani <reza3qorbani@gmail.com>
2021-06-28 15:17:09 +0200

    restrucre project tree

---
+++ include/mainwindow.h
@@ -0,0 +1,37 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <boost/asio.hpp>
+#include <QInputDialog>
+#include "client.hpp"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+
+private slots:
+
+    void on_actionConnect_to_Localhost_triggered();
+
+    bool on_pushButton_2_clicked();
+
+    void on_pushButton_pressed();
+
+private:
+    Ui::MainWindow *ui;
+    friend class chat_client;
+    friend void start_client (std::string host,  std::string port, MainWindow* mainwindow);
+};
+
+
+#endif // MAINWINDOW_H
--- src/server.cpp
+++ src/server.cpp
@@ -1,4 +1,12 @@
-
+//
+// chat_server.cpp
+// ~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2003-2021 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
 
 #include "server.hpp"
 
@@ -142,31 +150,3 @@ private:
 
 //----------------------------------------------------------------------
 
-int main(int argc, char* argv[])
-{
-    try
-    {
-        if (argc < 2)
-        {
-            std::cerr << "Usage: chat_server <port> [<port> ...]\n";
-            return 1;
-        }
-
-        boost::asio::io_context io_context;
-
-        std::list<chat_server> servers;
-        for (int i = 1; i < argc; ++i)
-        {
-            tcp::endpoint endpoint(tcp::v4(), std::atoi(argv[i]));
-            servers.emplace_back(io_context, endpoint);
-        }
-
-        io_context.run();
-    }
-    catch (std::exception& e)
-    {
-        std::cerr << "Exception: " << e.what() << "\n";
-    }
-
-    return 0;
-}
